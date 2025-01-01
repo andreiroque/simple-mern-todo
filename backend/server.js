ab2ee5567cc5a9e0f224236a2addfcde6c3d1def
Andrei Roque <roqueandrei0316@gmail.com>
2025-01-01 10:57:17 +0800

    Add connection to the database using mongoose

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose
+  .connect(process.env.MONGOOSE_URI, { directConnection: true })
+  .then(() => console.log("Successfully Connected to the Database!"))
+  .catch((err) => console.error(err));
